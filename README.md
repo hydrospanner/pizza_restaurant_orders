8b455f092b942f152750283998a5572e561b30c1
hydrospanner <johntuckerusa@gmail.com>
2018-08-05 10:31:40 -0500

    add user app with login logout

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Project 3
+# Pizza Restaurant
 
 Web Programming with Python and JavaScript
---
+++ users/views.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+# Create your views here.
+
+def user_view(request):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "users/user.html", context)
+
+def login_view(request):
+    if request.method == 'GET':
+        return render(request, "users/login.html")
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
