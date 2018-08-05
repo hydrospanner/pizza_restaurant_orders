3840a7a7441992ffc7c10200d0e0f9084c8ebb72
hydrospanner <johntuckerusa@gmail.com>
2018-08-05 13:47:23 -0500

    added signup form

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
+    path("signup", views.signup, name="signup"),
     path("user", views.user_view, name="user"),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -28,3 +29,17 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return render(request, "users/login.html", {"message": "Logged out."})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
