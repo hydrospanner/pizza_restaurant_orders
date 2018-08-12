6e660e0ca04dcbbee30efb1ec9e096216cbf32e1
hydrospanner <johntuckerusa@gmail.com>
2018-08-12 12:07:40 -0500

    add filters and order date sort to admin for orders

--- orders/admin.py
+++ orders/admin.py
@@ -3,9 +3,13 @@ from django.contrib import admin
 from .models import PizzaCrust, Pizza, Topping, Special, Order
 # Register your models here.
 
+class OrderAdmin(admin.ModelAdmin):
+    filter_horizontal = ('pizzas', )
+    list_filter = ('placed', 'fulfilled')
+    ordering = ['added_on']
 
 admin.site.register(PizzaCrust)
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Special)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
